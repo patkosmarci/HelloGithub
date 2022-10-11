26cb558fbae74362d13aaa738ea59f4e743e92b0
patkosmarci <68965060+patkosmarci@users.noreply.github.com>
2022-10-11 16:48:36 +0200

    glasses added

--- HelloGithub/Person.cs
+++ HelloGithub/Person.cs
@@ -11,5 +11,6 @@ namespace HelloGithub
         public string Name { get; set; }
         public int Age { get; set; }
         public string Job { get; set; }
+        public bool HaveGlasses { get; set; }
     }
 }
